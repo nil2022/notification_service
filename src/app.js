faf26b394aaa00c467413271138ea2fb8edc203f
Nilanjan Haldar <nil.haldar@gmail.com>
2025-11-09 00:45:19 +0530

    Add environment configuration file for server and database settings

--- src/app.js
+++ src/app.js
@@ -5,6 +5,9 @@ import securedHeaders from 'helmet';
 import limiter from './utils/api-rate-limiter.js';
 import logger from './utils/pinoLogger.js';
 import pinoHTTP from 'pino-http';
+import chalk from 'chalk';
+import notificationRouter from './routes/ticketNotification.route.js';
+import env from './configs/env.config.js';
 
 const app = express();
 
@@ -25,28 +28,33 @@ app.use(securedHeaders());
 app.use(pinoHTTP({ logger }));
 
 const connectDB = async () => {
-		const startTime = Date.now();
-		const connect = await mongoose.connect(process.env.DB_URL);
-		logger.info(`Time taken to connect to DB: ${Date.now() - startTime}ms`);
-		logger.info(`MongoDB Connected to Host: ${connect.connection.host}`);
+	const startTime = Date.now();
+	const connect = await mongoose.connect(env.DB_URL);
+	logger.info(`Time taken to connect to DB: ${Date.now() - startTime}ms`);
+	// logger.info(`MongoDB Connected to Host: ${connect.connection.host}`);
+	const { host, name: dbName } = connect.connection;
+	console.log(
+		chalk.bgGreen.black(
+			` MongoDB Connected to DB Host:-> ${host} , DB Name:-> ${dbName} `
+		)
+	);
 };
 
-import notificationRouter from './routes/ticketNotification.route.js';
 app.use('/api/v1/notify', notificationRouter);
 
 // FIRST CONNECT TO MONGODB THEN START LISTENING TO REQUESTS
 connectDB()
 	.then(() => {
-		app.listen(process.env.SERVER_PORT || 8000, () => {
+		app.listen(env.SERVER_PORT || 8000, () => {
 			logger.info(
-				`Notification service listening to PORT ${process.env.SERVER_PORT}`
+				`Notification service listening to PORT ${env.SERVER_PORT}`
 			);
 		});
 	})
 	.catch((err) => logger.error(err, "Can't connect to DB:")); // IF DB CONNECT FAILED, CATCH ERROR
 
 app.get('/', (_, res) => {
-    logger.info('Notification Service is up and Running !');
+	logger.info('Notification Service is up and Running !');
 	return res.status(200).json({
 		message: 'Notification Service is up and Running 👍🏻',
 		statusCode: 200,
---
+++ src/configs/env.config.js
@@ -0,0 +1,44 @@
+import { str, num, cleanEnv, port } from 'envalid';
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const envPath = path.resolve(__dirname, '../', '../', '.env');
+console.log('envPath: ', envPath);
+dotenv.config({ path: envPath });
+
+const envVariables = process.env;
+
+const env = cleanEnv(envVariables, {
+	//Server Configuration
+	SERVER_PORT: port(),
+	NODE_ENV: str({
+		choices: ['development', 'production'],
+		default: 'development'
+	}),
+
+	// ----------MongoDB URL---------
+	DB_URL: str(),
+
+	// ----------CRON-SCHEDULE------------
+	CRON_SCHEDULE: str({ default: '*/5 * * * *' }), // Every 5 minutes
+
+	// BREVO EMAIL CREDENTIALS
+	MAIL_HOST: str(),
+	MAIL_PORT: num(),
+	MAIL_USERNAME: str(),
+	MAIL_PASSWORD: str(),
+	MAIL_FROM: str(),
+	MAIL_REPLY_TO: str(),
+
+	// ##### EXPRESS-RATE-LIMIT CONFIGURATION #####
+	//  SPECIFY EXPRESS-RATE-LIMIT TIME IN MINUTES
+	RATE_LIMIT_TIME: num(),
+	//  SPECIFY MAXIMUM NO. OF REQUESTS PER IP ADDRESS
+	MAX_REQUESTS: num()
+});
+
+export default env;
